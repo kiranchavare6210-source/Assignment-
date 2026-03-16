1ec283126de33368bcfa1ebd56e3571cffcb0745
Kiran <kiranchavare6210@gmail.com>
2026-03-16 22:34:51 +0530

    Initial commit

--- test.js
+++ test.js
@@ -2,27 +2,23 @@ const { Builder, By, until } = require('selenium-webdriver');
 
 async function clickSubmitButton() {
 
-    // Launch Chrome browser
     let driver = await new Builder().forBrowser('chrome').build();
 
     try {
 
-        // Open website
         await driver.get('http://www.example.com/form-with-submit');
 
-        // Wait until the Submit button with correct text is found
+        // Wait for button using text
         let submitButton = await driver.wait(
-            until.elementLocated(By.xpath("//button[normalize-space()='Submit']")),
+            until.elementLocated(By.xpath("//button[contains(text(),'Submit')]")),
             10000
         );
 
-        // Wait until button is visible
+        // Wait until button is visible and clickable
         await driver.wait(until.elementIsVisible(submitButton), 10000);
-
-        // Wait until button is enabled
         await driver.wait(until.elementIsEnabled(submitButton), 10000);
 
-        // Click the button
+        // Click button
         await submitButton.click();
 
         console.log("Submit button clicked successfully");
@@ -33,10 +29,8 @@ async function clickSubmitButton() {
 
     } finally {
 
-        // Close browser
         await driver.quit();
     }
 }
 
-// Run function
 clickSubmitButton();
